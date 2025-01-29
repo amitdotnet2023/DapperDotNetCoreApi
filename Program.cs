072f7f5180177681206f7d0154e7bbff921f9f60
Amit <amitlight95@gmail.com>
2025-01-29 23:19:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using DapperDotNetCoreApi.Context;
+using DapperDotNetCoreApi.Contracts;
+using DapperDotNetCoreApi.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
